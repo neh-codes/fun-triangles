2498a014deda39e7b0e27c76c3da52d406415519
neh-codes <nehcodes@gmail.com>
2022-09-05 21:20:01 +0530

    Update Hypotenuse

--- hypotenuse.js
+++ hypotenuse.js
@@ -2,6 +2,13 @@ const sides = document.querySelectorAll(".side-input");
 const isHypotenuseBtn = document.querySelector("#hypotenuse-btn");
 const outputEl = document.querySelector("#output");
 
+function validate() {
+  if (sides[0].value === "" || sides[1].value === "")
+    output.innerText = "Please enter both values";
+  else if (Number(sides[0].value <= 0) || Number(sides[1].value <= 0))
+    output.innerText = "Both values should be greater than zero";
+  else calculateHypotenuse(Number(sides[0].value), Number(sides[1].value));
+}
 function calculateSumOfSquares(a, b) {
   const SumOfSquares = a * a + b * b;
   return SumOfSquares;
@@ -16,4 +23,4 @@ function calculateHypotenuse() {
   outputEl.innerText = "Length of Hypotenuse is " + lengthOfHypotenuse;
 }
 
-isHypotenuseBtn.addEventListener("click", calculateHypotenuse);
+isHypotenuseBtn.addEventListener("click", validate);
